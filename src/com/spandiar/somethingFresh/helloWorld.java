f38f310a1b318518b68febc169cd0e96e69be0f7
sankar_pandiarajan <spandiarajan@apple.com>
2019-08-26 20:13:58 -0700

    check this first program

---
+++ src/com/spandiar/somethingFresh/helloWorld.java
@@ -0,0 +1,16 @@
+package com.spandiar.somethingFresh;
+
+public class helloWorld {
+
+	public helloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!!! I'm gonna crack Java this time");
+
+	}
+
+}
